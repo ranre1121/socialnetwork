d744ef51259935689ccacf56f4b96b6f311e21d4
ranre1121 <pognar1121@gmail.com>
2025-10-07 17:07:44 +0500

    d

--- client/src/sections/Messages.tsx
+++ client/src/sections/Messages.tsx
@@ -4,32 +4,7 @@ import Chat from "./Chat";
 import profilePlaceholder from "../../public/images/profile-placeholder.png";
 import { useLocation } from "react-router-dom";
 import type { Conversation } from "../types/Types";
-
-function formatMessageTime(dateString: string | undefined): string {
-  if (!dateString) return "";
-  const date = new Date(dateString);
-  const now = new Date();
-
-  const isToday =
-    date.getDate() === now.getDate() &&
-    date.getMonth() === now.getMonth() &&
-    date.getFullYear() === now.getFullYear();
-
-  const yesterday = new Date();
-  yesterday.setDate(now.getDate() - 1);
-  const isYesterday =
-    date.getDate() === yesterday.getDate() &&
-    date.getMonth() === yesterday.getMonth() &&
-    date.getFullYear() === yesterday.getFullYear();
-
-  if (isToday) {
-    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-  }
-  if (isYesterday) {
-    return "Yesterday";
-  }
-  return date.toLocaleDateString("en-US", { month: "short", day: "numeric" }); // e.g. Sep 28
-}
+import { formatMessageTime } from "../utils/messagesUtils";
 
 const Messages = () => {
   const { user } = useUser();
